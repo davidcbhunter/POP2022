fe0c6b0ce8966dcd4b797db38e63269ededc743d
davidcbhunter <hunter.davidcb@gmail.com>
2022-05-17 14:29:16 +0900

    Add files via upload

---
+++ instance.py
@@ -0,0 +1,30 @@
+name = "Tom"
+age = 1000
+is_hungry = True
+weight = 12.098
+
+#print(id(name))
+#print(id(age))
+#print(id(is_hungry))
+#print(id(weight))
+
+#print(id(type(name)))
+#print(id(type(age)))
+#print(id(type(is_hungry)))
+#print(id(type(weight)))
+
+
+def combine(a,b):
+    print(a+b)
+
+combine("Yellow","Blue")
+combine(34,41)
+combine("34","41")
+
+g = "3.1415926535........"
+print(type(g))
+
+def to_dog_age(age):
+    print(age * 7)
+
+to_dog_age(age)
