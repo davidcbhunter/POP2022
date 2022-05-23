3914839f1ba8023735308fa2e5b98a3afc3069aa
davidcbhunter <hunter.davidcb@gmail.com>
2022-05-23 11:00:13 +0900

    Add files via upload

--- visual novel.py
+++ visual novel.py
@@ -1,13 +1,13 @@
-dialogue = "Main_Character: How are you, Side_Character? \n \
-    Side_Character:Oh, not bad, Main_Character. And you? \n \
-    Main_Character: Pretty good! So, I wanted to ask you something. \n \
-    Side_Character: Sure, Main_Character. Go ahead! \n \
-    Main_Character: Do you have any plans for SEASON vacation? \n \
-    Side_Character: Not really. Why \n \
-    Main_Character: Well, Side_Character, I was thinking, how about \n \
-    we go ACTION in PLACE? I hear there THING! \n \
-    Side_Character: Oh, Main_Character! That sounds really fun! \n \
-    Main_Character: Great! Let's start thinking about when we can go. \n \
-    Side_Character: OK, Main_Character. How about TIME? \n \
-    Main_Character: That works for me, Side_Character. \n \
-    Side_Character: OK, Main_Character. TIME works for me! \n \"
+dialogue = "Main_Character: How are you, Side_Character? \n\
+Side_Character:Oh, not bad, Main_Character. And you? \n\
+Main_Character: Pretty good! So, I wanted to ask you something. \n\
+Side_Character: Sure, Main_Character. Go ahead! \n\
+Main_Character: Do you have any plans for SEASON vacation? \n\
+Side_Character: Not really. Why \n\
+Main_Character: Well, Side_Character, I was thinking, how about \n\
+we go ACTION in PLACE? I hear there THING! \n\
+Side_Character: Oh, Main_Character! That sounds really fun! \n\
+Main_Character: Great! Let's start thinking about when we can go. \n\
+Side_Character: OK, Main_Character. How about TIME? \n\
+Main_Character: That works for me, Side_Character. \n\
+Side_Character: OK, Main_Character. TIME works for me!"
