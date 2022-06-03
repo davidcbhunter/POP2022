7bda8d5f74e69acde3367b15d76d01a1ad8505a3
davidcbhunter <hunter.davidcb@gmail.com>
2022-06-03 16:32:23 +0900

    Add files via upload

---
+++ function practice.py
@@ -0,0 +1,20 @@
+name = "david charles beattie hunter"
+
+print(name)
+
+
+cap_name = name.find("er")
+
+print(cap_name)
+
+number = 1234567890
+
+my_height = 46575.9443766
+
+number2 = my_height
+
+print(number2)
+
+is_Japanese = True
+
+
