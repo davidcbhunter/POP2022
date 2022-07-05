07bb2765dc736d798b615def9922ed6b8b66eadf
davidcbhunter <hunter.davidcb@gmail.com>
2022-07-05 14:42:55 +0900

    Add files via upload

--- Vending Machine Example.py
+++ Vending Machine Example.py
@@ -14,7 +14,7 @@ amount1 = 50
 amount2 = 50
 amount3 = 40
 amount4 = 30
-amount4 = 20
+amount5 = 20
 
 money = 2000
 
@@ -26,7 +26,14 @@ def ShowProducts():
 
 def IsProduct(cp):
     return cp == product1 or cp == product2 \
-   or cp == product3 or cp != product5 or cp == product5
+   or cp == product3 or cp == product4 or cp == product5
+
+def IsAmountEnough(cp):
+    return (cp == product1 and amount1 > 0) or \
+           (cp == product2 and amount2 > 0) or \
+           (cp == product3 and amount3 > 0) or \
+           (cp == product4 and amount4 > 0) or \
+           (cp == product5 and amount5 > 0)
 
 def IsEnoughMoney(cp, cm):
     return (cp == product1 and cm >= price1) or \
@@ -34,6 +41,11 @@ def IsEnoughMoney(cp, cm):
         (cp == product3 and cm >= price3) or \
         (cp == product4 and cm >= price4) or \
         (cp == product5 and cm >= price5)
+
+def ShowMessage(cp,cm,p):
+    if cm > p:
+        print("You have " + str(cm-p) + " yen in change.")
+    print("Enjoy your " + cp)
 
 def Buy(cp, cm):
     global amount1
@@ -45,37 +57,35 @@ def Buy(cp, cm):
     if cp == product1 and cm >= price1:
         amount1 -= 1
         money += price1
-        print("You have " + str(cm-price1) + \
-              " yen in change. \n Enjoy your " + cp)
+        ShowMessage(cp,cm,price1)
     if cp == product2 and cm >= price2:
         amount2 -= 1
         money += price2
-        print("You have " + str(cm-price2) + \
-              " yen in change. \n Enjoy your " + cp)
+        ShowMessage(cp,cm,price2)
     if cp == product3 and cm >= price3:
         amount3 -= 1
         money += price3
-        print("You have " + str(cm-price3) + \
-              " yen in change. \n Enjoy your " + cp)
+        ShowMessage(cp,cm,price3)
     if cp == product4 and cm >= price4:
         amount4 -= 1
         money += price4
-        print("You have " + str(cm-price4) + \
-              " yen in change. \n Enjoy your " + cp)
+        ShowMessage(cp,cm,price4)
     if cp == product5 and cm >= price5:
         amount5 -= 1
         money += price5
-        print("You have " + str(cm-price5) + \
-              " yen in change. \nEnjoy your " + cp + "!")
+        ShowMessage(cp,cm,price5)
+
 ShowProducts()
 
 current_product = input("What product do you want to buy? \n")
 
 if not IsProduct(current_product):
-    print("Not a product")
+    print("Not a product.")
+elif not IsAmountEnough(current_product):
+    print("Sorry. We are sold out.")
 else:
     current_money = int(input("Insert money. \n"))
     if not IsEnoughMoney(current_product, current_money):
-        print("Not enough money")
+        print("Not enough money.")
     else:
         Buy(current_product, current_money)
