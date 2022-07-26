388958ca4123d2d07c961e1ceeb13e2f1a8d460a
davidcbhunter <hunter.davidcb@gmail.com>
2022-07-26 14:27:00 +0900

    Add files via upload

--- Vending Machine Example.py
+++ Vending Machine Example.py
@@ -1,24 +1,9 @@
-#we will delete these later
-product1 = ""
-product2 = ""
-product3 = ""
-product4 = ""
-product5 = ""
-
 product_list = ["Black Coffee","Milk Coffee",\
                 "Apple Juice","Cola","Energy Drink"]
 
-price1 = 130
-price2 = 130
-price3 = 160
-price4 = 160
-price5 = 220
+price_list = [130,130,160,160,220]
 
-amount1 = 40
-amount2 = 50
-amount3 = 40
-amount4 = 30
-amount5 = 20
+amount_list = [40,50,40,30,20]
 
 money = 2000
 
@@ -27,8 +12,8 @@ def ShowProducts():
     # of the products or prices
     print(product_list[0] + "\t" + product_list[1] + "\t" + product_list[2] + "\t" \
           + product_list[3] + "\t" + product_list[4])
-    print(str(price1) + "\t\t" + str(price2) + "\t\t" + str(price3) + "\t\t" \
-          + str(price4) + "\t" + str(price5))
+    print(str(price_list[0]) + "\t\t" + str(price_list[1]) + "\t\t" + str(price_list[2]) + "\t\t" \
+          + str(price_list[3]) + "\t" + str(price_list[4]))
 
 def IsProduct():
     #we don't need global here because we are not changing the value
@@ -36,53 +21,26 @@ def IsProduct():
     return current_product in product_list
 
 def IsAmountEnough():
-    return (current_product == product1 and amount1 > 0) or \
-           (current_product == product2 and amount2 > 0) or \
-           (current_product == product3 and amount3 > 0) or \
-           (current_product == product4 and amount4 > 0) or \
-           (current_product == product5 and amount5 > 0)
+    return amount_list[product_index] > 0
 
 def IsEnoughMoney():
-    return (current_product == product1 and current_money >= price1) or \
-        (current_product == product2 and current_money >= price2) or \
-        (current_product == product3 and current_money >= price3) or \
-        (current_product == product4 and current_money >= price4) or \
-        (current_product == product5 and current_money >= price5)
+    #price_list[product_index] <= current_money
+    return current_money >= price_list[product_index]
 
-def ShowMessage(price):
-    if current_money > price:
+def ShowMessage():
+    if current_money > price_list[product_index]:
         #we only want to print the change message if the customer
         # gave us too much money.
-        print("You have " + str(current_money-price) + " yen in change.")
+        print("You have " + str(current_money-price_list[product_index]) + " yen in change.")
     print("Enjoy your " + current_product + ".")
 
 def Buy():
-    global amount1
-    global amount2
-    global amount3
-    global amount4
-    global amount5
+    global amount_list
     global money
-    if current_product == product1:
-        amount1 -= 1
-        money += price1
-        ShowMessage(price1)
-    if current_product == product2:
-        amount2 -= 1
-        money += price2
-        ShowMessage(price2)
-    if current_product == product3:
-        amount3 -= 1
-        money += price3
-        ShowMessage(price3)
-    if current_product == product4:
-        amount4 -= 1
-        money += price4
-        ShowMessage(price4)
-    if current_product == product5:
-        amount5 -= 1
-        money += price5
-        ShowMessage(price5)
+    amount_list[product_index] -= 1
+    money += price_list[product_index]
+    
+    ShowMessage()
 
 ShowProducts()
 
@@ -90,11 +48,13 @@ current_product = input("What product do you want to buy? \n")
 
 if not IsProduct():
     print("Not a product.")
-elif not IsAmountEnough():
-    print("Sorry. We are sold out.")
 else:
-    current_money = int(input("Insert money. \n"))
-    if not IsEnoughMoney():
-        print("Not enough money.")
+    product_index = product_list.index(current_product)
+    if not IsAmountEnough():
+        print("Sorry. We are sold out.")
     else:
-        Buy()
+        current_money = int(input("Insert money. \n"))
+        if not IsEnoughMoney():
+            print("Not enough money.")
+        else:
+            Buy()
