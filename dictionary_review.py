2e586864e1075dfc5c6bf2923c5499ecdc5937f6
davidcbhunter <hunter.davidcb@gmail.com>
2022-11-08 14:26:40 +0900

    Add files via upload

---
+++ dictionary_review.py
@@ -0,0 +1,12 @@
+my_list = ["B","C","A"]
+
+print(my_list[2])
+
+my_dictionary = {"b":"B","c":"C","a":"A"}
+
+my_dictionary["d"] = "D"
+
+print(my_dictionary["a"])
+
+
+
--- file_practice.py
+++ file_practice.py
@@ -3,8 +3,36 @@ my_file = open("Movies.txt", "r",encoding="utf-8")
 
 info = my_file.read()
 
+#check the type of info
+#print(type(info))
+
 my_file.close()
 
 li = info.splitlines()
 
-print(li)
+#check the type of li
+#print(type(li))
+
+#for loop --> prints each line in li
+
+movie_dictionary = {}
+
+for x in li:
+    #print(x)
+    sp = x.split(",")
+    #print each item in sp
+    #for s in sp:
+    #    print(s)
+    
+    #check the type of x
+    #print(type(x))
+    #print(type(sp))
+    #check the type of sp[0]
+    #print(type(sp[0]))
+    #check the type of sp[1]
+    #print(type(sp[1]))
+    
+    movie_dictionary[int(sp[0])] = sp[1]
+
+print(movie_dictionary[2017])
+
