b8684dcea41b9c9b5080307e40a63cef254cb8b5
davidcbhunter <hunter.davidcb@gmail.com>
2023-01-06 11:06:27 +0900

    Add files via upload

--- Memory Game.py
+++ Memory Game.py
@@ -4,36 +4,64 @@ import winsound
 
 #make a Trump class
 class Trump:
+    # we should also give each card a position,x
     def __init__(self,btn,b_file_name,f_file_name,t_name):
         self.button = btn
         self.trump_back_file_name = b_file_name
         self.trump_front_file_name = f_file_name
         self.trump_name = t_name
         self.is_showing_face = False
-        self.button.configure(text = "back")
+
+
+        #self.position = x
+        
+        back_image = tk.PhotoImage(file = self.trump_back_file_name)
+        # we need to set the image both ways, because
+        self.button.configure(image = back_image)
+        self.button.image = back_image
+        # compund lets us have both text and images together
+        self.button.configure(command = self.Flip, compound = "center")
 
     def Flip(self):
         
         self.is_showing_face = not self.is_showing_face
+        # play a sound
+        
         if self.is_showing_face == False:
-            #back_image = tk.PhotoImage(file = self.trump_back_file_name)
-            #self.button.configure(image = back_image)
-            #self.button.image = back_image
-            self.button.configure(text = "Back")
+            #we are showing the back side
+            back_image = tk.PhotoImage(file = self.trump_back_file_name)
+            self.button.configure(text = "")
+            self.button.configure(image = back_image)
+            self.button.image = back_image
+            #if we are showing the back side, we should enable this button
+
         else:
-            #self.is_showing_face = False
+            #we are showing the face
+            # we make a small image so that we can force the button size
+            # using pixels instead of lines and letters
+            img = tk.PhotoImage(width=1, height=1)
+            # we need to set the image both ways, because
+            self.button.configure(image = img)
+            self.button.image = img
             self.button.configure(text = self.trump_name)
-            #self.button.image = None
+            self.button.configure(width = card_width, height = card_height)
+
+            #if we are showing the face, we should disable this button
             
+            # if we are showing the face, we should check firstChoice and secondChoice
+            # another function!
+
+        
+
         
 
 # create a window    
 root = tk.Tk()
 
-root.geometry("512x512")
-#root.title("Memory Game")
+root.geometry("1024x1024")
+root.title("Memory Game")
 
-back_image = tk.PhotoImage(file = "playing card back.png")
+#back_image = tk.PhotoImage(file = "playing card back.png")
 
 def LoadCards():
     cards = []
@@ -51,10 +79,13 @@ cards = LoadCards()
 
 number_of_columns = 5
 number_of_rows = 4
+
+#card x information
 card_width = 113
 x_offset = 129
 x_spacing = 50
 
+#card y information
 card_height = 166
 y_offset = 105
 y_spacing = 50
@@ -65,8 +96,11 @@ number_of_unique_cards = number_of_cards // 2
 
 random.shuffle(cards)
 
+#make function to pick the number_of_unique_cards,
+#double them, and shuffle them
 
 
+# put this in a function too
 for x in range(number_of_cards):
     x_pos = x_offset + (x % number_of_columns)*card_width +\
             (x % number_of_columns)*x_spacing
@@ -77,22 +111,23 @@ for x in range(number_of_cards):
     btn.place(x = x_pos, y = y_pos)
 
     t = Trump(btn,"playing card back.png","f.txt",cards[x])
-    btn.configure(command = t.Flip)
 
-#btn = tk.Button(root)#,image = back_image,compound = "center")
-#btn.place(x = 0, y = 0)
+# we need to save each card in a dictionary or list
 
-#card size is 113 by 166
+# we need to save the two players' scores
 
-#t = Trump(btn,"playing card back.png","f.txt","Ace of Clubs")
-#btn.configure(command = t.Flip)
+# we need to know whose turn it is
 
-#let's print the information in t
-#print(t.button)
-#print(t.trump_back_file_name)
-#print(t.trump_front_file_name)
-#print(t.trump_name)
-#print(t.is_showing_face)
+# when a card is clicked, we need to check firstChoice and secondChoice
+# if this is the secondChoice, we check
+# if they match, give points to the current player AND
+# remove the cards from the screen, AND
+# remove the cards from the list/dictionary
+# if they don't match, just flip firstChoice and secondChoice
 
+# check how many cards are left
+# if there are no cards, the game is over.
+# Say who the winner is.
+# play a sound
 
-
+#reset button?
