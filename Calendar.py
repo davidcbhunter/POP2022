4bd77be39c51ec30be3385501f2b006ea192ea28
davidcbhunter <hunter.davidcb@gmail.com>
2023-02-14 14:24:15 +0900

    Add files via upload

--- Calendar.py
+++ Calendar.py
@@ -9,17 +9,52 @@ root.geometry("500x500")
 
 root.title("Calandar")
 
-d = datetime.date.today()
+date = datetime.date.today()
 
-prev_Month = tk.Button(root,text = "Previous")
+def NextMonth():
+    print("next")
+    global current_month
+    global current_year
+    global month_label
+    current_month += 1
+    if current_month > 12:
+        current_month = 1
+        current_year += 1
+    #print(current_month)
+    days_in_month = monthrange(current_year,current_month)
+    day_of_week = datetime.date(current_year,current_month,1).weekday()
+    date = datetime.date(current_year,current_month,1)
+    month = date.strftime("%B")
+    month_label.configure(text = month)
+    ShowMonth(days_in_month,day_of_week)
+
+
+def PreviousMonth():
+    print("previous")
+    global current_month
+    global current_year
+    global month_label
+    current_month -= 1
+    if current_month < 1:
+        current_month = 12
+        current_year -= 1
+    days_in_month = monthrange(current_year,current_month)
+    day_of_week = datetime.date(current_year,current_month,1).weekday()
+    date = datetime.date(current_year,current_month,1)
+    month = date.strftime("%B")
+    month_label.configure(text = month)
+    ShowMonth(days_in_month,day_of_week)
+
+
+prev_Month = tk.Button(root,text = "Previous", command = PreviousMonth)
 
 prev_Month.grid(column = 0, row = 0)
 
-next_Month = tk.Button(root,text = "Next")
+next_Month = tk.Button(root,text = "Next", command = NextMonth)
 
 next_Month.grid(column = 6, row = 0)
 
-month = d.strftime("%B")
+month = date.strftime("%B")
 month_label = tk.Label(root,text = month)
 month_label.grid(column=0,row=1,columnspan=6)
 
@@ -28,20 +63,29 @@ for day in days:
     db = tk.Label(root,text = day)
     db.grid(column = days.index(day),row = 2)
 
-days_in_month = monthrange(d.year,d.month)
-print(days_in_month)
-print(type(days_in_month[1]))
-day_of_week = datetime.date(d.year,d.month,1).weekday()
-print(day_of_week)
-print(type(day_of_week))
-
-
-
 date_labels = []
-def ShowMonth(mon):
-    date_labels.clear()
+for da in range(35):
+    lb = tk.Label(root,text = "")
+    date_labels.append(lb)
+
+def ShowMonth(days_in_month,day_of_week):
+    if len(date_labels) > 0:
+        for da in date_labels:
+            da.configure(text = "")
     for days in range(days_in_month[1]):
-        lb = tk.Label(root,text = str(days+1))
+        lb = date_labels[day_of_week+days]
+        lb.configure(text = str(days+1))
         lb.grid(column = (day_of_week+days)%7, row = 3 + (days+day_of_week)//7)
-        date_labels.append(lb)
+
+
+current_month = date.month
+current_year = date.year
+
+days_in_month = monthrange(current_year,current_month)
+day_of_week = datetime.date(current_year,current_month,1).weekday()
+ShowMonth(days_in_month,day_of_week)
+
+
+    
+
 
